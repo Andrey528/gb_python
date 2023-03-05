dae847ff7c145a842dd9f3c9ca8e3b95de52b512
Andrew <telepnev.1997@mail.ru>
2023-03-05 14:51:55 +0700

    gb_hw_5

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from py_hw_3 import *
 from py_hw_4 import *
 from redis_bootcamp import *
 from opencv_bootcamp import *
+from quick_sort import *
+from merge_sort import *
+from py_hw_5 import *
 
 # hw_1()
 # hw_2()
@@ -12,4 +15,8 @@ from opencv_bootcamp import *
 # redis_bootcamp_server()
 # redis_bootcamp_client()
 # cv_work_with_img()
-cv_work_with_face_detection()
+# cv_work_with_face_detection()
+# array = [1, 4, 56, 1, 512, 12, 12, 14, 2, 1, 7]
+# print(quick_sort(array))
+# print(merge_sort(array))
+hw_5()
